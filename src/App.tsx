bd6638486c08dbb8a0f03eb88bea40237a8d92e7
Kahffi <daffaalkahffi24@gmail.com>
2024-10-07 21:58:57 +0700

    forward chaining implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import { useState } from "react";
 import { Questions } from "./Constant";
 import Question from "./components/Question";
 import useRule from "./hooks/useRule";
+import useFacts from "./hooks/useFacts";
 
 function App() {
   const [answers, setAnswers] = useState<boolean[]>(Questions.map(() => false));
   const { rules } = useRule();
+  const { facts, doForwardChaining, inferredFacts } = useFacts(rules);
 
   function toggleAnswer(idx: number): void {
     const temp = [...answers];
     temp[idx] = !temp[idx];
     setAnswers(temp);
   }
-
-  console.log(rules);
 
   return (
     <div className="bg-slate-800 min-h-screen">
@@ -29,10 +29,18 @@ function App() {
         </div>
         <button
           type="button"
+          onClick={() => {
+            doForwardChaining(answers);
+          }}
           className="py-3 px-10 w-44 bg-slate-700 text-white rounded-md"
         >
           Evaluate
         </button>
+        {facts.size > 0 && (
+          <p className="text-2xl">
+            {facts} <span>{inferredFacts}</span>
+          </p>
+        )}
       </div>
     </div>
   );
--- src/hooks/useFacts.ts
+++ src/hooks/useFacts.ts
@@ -1,5 +1,55 @@
 import { useState } from "react";
+import { Rule } from "./useRule";
+import { ANSWERS_MAP } from "../Constant";
 
-export default function useFacts() {
-  const [facts, setFacts] = useState<string[]>([]);
+const factsHasAll = (list: string[], facts: Set<string>) =>
+  list.every((item) => facts.has(item));
+
+export default function useFacts(rules: Rule[]) {
+  const [facts, setFacts] = useState<Set<string>>(new Set([]));
+  const [inferredFacts, setInferredFacts] = useState<Set<string>>(new Set([]));
+
+  //   function populateFacts(answers: boolean[]) {
+  //     setFacts(
+  //       answers.reduce((set, ans, idx) => {
+  //         if (ans) {
+  //           set.add(ANSWERS_MAP[idx]);
+  //         }
+  //         return set;
+  //       }, new Set<string>([]))
+  //     );
+  //   }
+
+  function doForwardChaining(answers: boolean[]) {
+    const tempInferFacts = new Set<string>([]);
+    // const tempFacts = new Set<string>(facts);
+
+    const tempFacts = answers.reduce((set, ans, idx) => {
+      if (ans) {
+        set.add(ANSWERS_MAP[idx]);
+      }
+      return set;
+    }, new Set<string>([]));
+
+    while (true) {
+      let inferred = false;
+      for (const rule of rules) {
+        if (
+          factsHasAll(rule.antecedent, tempFacts) &&
+          !tempInferFacts.has(rule.consequent)
+        ) {
+          tempFacts.add(rule.consequent);
+          tempInferFacts.add(rule.consequent);
+          inferred = true;
+          break;
+        }
+      }
+      if (!inferred) break;
+    }
+
+    setFacts(tempFacts);
+    setInferredFacts(tempInferFacts);
+  }
+
+  return { facts, doForwardChaining, inferredFacts };
 }
